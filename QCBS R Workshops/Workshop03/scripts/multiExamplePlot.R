515177892e3c88b7146c29ada3e3e960955a056e
RemiMaglione <remi.maglione@gmail.com>
2018-09-26 13:11:45 -0400

    last changes

--- QCBS R Workshops/Workshop03/scripts/multiExamplePlot.R
+++ QCBS R Workshops/Workshop03/scripts/multiExamplePlot.R
@@ -1,37 +1,19 @@
-library(gridBase)
-library(grid)
-
-
 # create an apporpriate viewport.  Modify the dimensions and coordinates as needed
-vp.BottomRight <- viewport(height= unit(0.5, "npc"), 
-                           width = unit(0.5, "npc"), 
-                           just=c("left","top"), 
-                           y=0.5, x=0.5)
+commm.h = .39 # This will change the "Height" of the ggplot graphs
+y = 0.42 # This will change the relative position on the "y" axis of the graph output
+vp.BottomRight <- viewport(height= unit(commm.h, "npc"),
+                           width = unit(0.333, "npc"),
+                           just  = c("left","top"),
+                           y = y, x = .333)
 
-vp.BottomLeft <- viewport(height= unit(0.5, "npc"), 
-                          width = unit(0.5, "npc"), 
-                          just=c("left","top"), 
-                          y=0.5, x=0)
+vp.bbright <- viewport(height= unit(commm.h, "npc"),
+                       width = unit(0.333, "npc"),
+                       just  = c("left","top"),
+                       y = y, x = .666)
 
-vp.BottomRight <- viewport(height= unit(0.5, "npc"), 
-                           width = unit(0.5, "npc"), 
-                           just=c("left","top"), 
-                           y=1, x=0.5)
-
-commm.h = .5
-vp.BottomRight <- viewport(height= unit(commm.h, "npc"), 
-                           width = unit(0.333, "npc"), 
-                           just  = c("left","top"), 
-                           y = 0.5, x = .333)
-
-vp.bbright <- viewport(height= unit(commm.h, "npc"), 
-                       width = unit(0.333, "npc"), 
-                       just  = c("left","top"), 
-                       y = 0.5, x = .666)
-
-# plot your base graphics 
+# plot your base graphics
 par(mfrow=c(2,3))
-plot(iris[,c("Sepal.Length","Sepal.Width")], main = "Density plot", 
+plot(iris[,c("Sepal.Length","Sepal.Width")], main = "Density plot",
      pch = 21, col = iris$Species,
      bg = iris$Species)
 abline(lm(iris[iris$Species =="setosa",c("Sepal.Width","Sepal.Length")]),    col = "black")
@@ -39,26 +21,29 @@ abline(lm(iris[iris$Species =="versicolor",c("Sepal.Width","Sepal.Length")]),col
 abline(lm(iris[iris$Species =="virginica",c("Sepal.Width","Sepal.Length")]), col = "green" )
 
 plot(density(iris[,c("Sepal.Length"),]), main = "Density plot")
-hist(iris[,c("Sepal.Length")], 
-     main = "Histogram", 
-     xlab = "Sepal.Length", 
+hist(iris[,c("Sepal.Length")],
+     main = "Histogram",
+     xlab = "Sepal.Length",
      breaks = 12)
-boxplot(iris[,c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")], 
-        main = "Boxplot", 
-        xlab = "Traits", 
+boxplot(iris[,c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")],
+        main = "Boxplot",
+        xlab = "Traits",
         breaks = 12)
 
 #create your ggplot object
-p <- ggplot(data = iris,aes(x = Sepal.Length, 
-                            y = Sepal.Width, 
-                            col = Species)) + geom_point() + 
-  geom_smooth(method = "lm") + theme(legend.position="none")
+p <- ggplot(data = iris,aes(x = Sepal.Length,
+                            y = Sepal.Width,
+                            col = Species)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme(legend.position="none")
 
-q <- ggplot(data = iris,aes(x = Species, 
-                            y = Sepal.Width, 
+q <- ggplot(data = iris,aes(x = Species,
+                            y = Sepal.Width,
                             col = Species,
-                            fill = Species)) + 
-  geom_violin() + theme(legend.position="none")
+                            fill = Species)) +
+  geom_violin() +
+  theme(legend.position="none")
 
 # plot the ggplot using the print command
 print(p, vp=vp.BottomRight)
